cc64b99fbbeb17d69b74ca3cf4e75c9dc69aa27b
Matei <olteanmat@gmail.com>
2025-02-11 21:04:59 +0200

    add logica.py

---
+++ logica.py
@@ -0,0 +1,42 @@
+# nr = 31
+#
+# paritate = nr % 2 == 0
+#
+#
+# if paritate:
+#     print("Nr par")
+# elif nr > 20:
+#     print("Nr mai mare ca 20")
+# else:
+#     print("Nu stiu")
+
+
+# nr = 10
+#
+# while nr > 0:
+#     nr -= 1
+#     if nr == 9:
+#         pass
+#     if nr == 7:
+#         continue
+#     elif nr == 4:
+#         break
+#     print(f'nr {nr}')
+
+
+# def sum_rec(n):
+#     """Calculeaza suma tuturor numerelor pana la n"""
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + sum_rec(n - 1)
+#
+#
+# print(sum_rec(3))
+
+
+
+
+
+
+
