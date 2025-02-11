b2e6ca73bd36e4f69a52216b099929751ed05a4e
Matei <olteanmat@gmail.com>
2025-02-11 07:06:10 +0200

    change wording on tema1

--- tema1.py
+++ tema1.py
@@ -10,25 +10,25 @@ lista_mea.sort(reverse=True)
 
 print(f'Lista sortata descendent: {lista_mea}')
 
-# # afisam elementele pare din lista
-# elemente_pare = []
-# lista_mea.sort()
-# for z in lista_mea:
-#     if z % 2 == 0:
-#         elemente_pare.append(z)
-# print(f'Elemente pare din lista: {elemente_pare}')
-#
-# # afisam elementele impare din lista
-# elemente_impare = []
-# for z in lista_mea:
-#     lista_mea.sort()
-#     if z % 2 != 0:
-#         elemente_impare.append(z)
-# print(f'Elemente impare din lista: {elemente_impare}')
+# afisam elementele pare din lista
+elemente_pare = []
+lista_mea.sort()
+for z in lista_mea:
+    if z % 2 == 0:
+        elemente_pare.append(z)
+print(f'Elemente pare din lista: {elemente_pare}')
+
+# afisam elementele impare din lista
+elemente_impare = []
+for z in lista_mea:
+    lista_mea.sort()
+    if z % 2 != 0:
+        elemente_impare.append(z)
+print(f'Elemente impare din lista: {elemente_impare}')
 
 lista_mea.sort()
 
-print(f'Lista mea {lista_mea}')
+print(f'Lista mea ordonata {lista_mea}')
 
 print(f'Elemente pare din lista ordonata: {lista_mea[1::2]}')
 
